161f7b5bafc0ae681ef8d78620271f35f031704b
Tom Schoolmeesters <tom.schoolmeesters@hotmail.com>
2025-07-10 16:51:57 +0200

    Add documentation deployment 2

--- docs/build/search_index.js
+++ docs/build/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[]
+[{"location":"#EU*grid*operations.jl","page":"Home","title":"EUgridoperations.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Welcome to the documentation for EU_grid_operations.jl.","category":"page"},{"location":"","page":"Home","title":"Home","text":"```@meta CurrentModule = EUgridoperations","category":"page"}]
 }
