dbadb12d7f93c9d49d4982d34429426755735708
Tom Schoolmeesters <tom.schoolmeesters@hotmail.com>
2025-07-10 16:57:53 +0200

    Add doc 3

--- docs/make.jl
+++ docs/make.jl
@@ -3,17 +3,16 @@ using Documenter, EU_grid_operations
 makedocs(
     modules = [EU_grid_operations],
     sitename = "EU_grid_operations",
-    warnonly = :missing_docs,
     format = Documenter.HTML(),
     pages = [
         "Home" => "index.md"
-     ]
+    ]
 )
 
 deploydocs(
     repo = "github.com/tomschoolmeesters/EU_grid_operations_thesis.git",
+    branch = "gh-pages",
+    devbranch = "main",  # change if your main branch has a different name
     target = "build",
-    branch = "main",
-    devbranch = "main",
     versions = nothing
 )
